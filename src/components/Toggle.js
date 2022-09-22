7964e1b39f717e74018e52712ca1d82a179da508
ELIUS <eliuswanyoro@gmail.com>
2022-09-22 17:33:13 +0300

    Done

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
+
+
+//decide whether we need state 
+//Is it passed in from a parent via props? If so, it probably isn’t state.
+//Can you compute it based on any other state or props in your component? If so, it isn’t state.
+//Does it remain unchanged over time? If so, it probably isn’t state
+
+
 
 function Toggle() {
-  return <button>OFF</button>;
+
+  const [isOn, setIsOn]= useState(false);
+
+  function handleClick(){
+    setIsOn((isOn) => !isOn);
+
+  }
+
+  const color = isOn ? "red" : "white"
+   
+  return <button  style={{background: color }} onClick={handleClick}>{isOn? "ON" : "OFF" }</button>;
 }
 
 export default Toggle;
